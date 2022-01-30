714bd75ebc18e310560981322b2d4d33e2fe95b6
vashvets <vashvets@0utlook.com>
2022-01-30 13:30:26 +0000

    modify script.

--- show_user_info_and_data.sh
+++ show_user_info_and_data.sh
@@ -3,18 +3,16 @@
 USERNAME=$1
 DIR_PATH=$2
 
-#if [[ '$1' == "root" ]]; then
-#echo "Ошибка"
-#elif [[ '$1' != "root" ]]; then
-#echo "all good"
-#fi;
 case $1 in
 root)
-echo "Ошибка-пользователь root"
+echo "Finding “root” user data is not allowed!"
 exit 42 ;;
 *)
 echo "Директории пользователя" $1
 esac 
-find $2 -user $1
+PROC=$(find $2 -user $1)
+echo $PROC is found!
 echo "Процессы пользователя"  $1
+
 ps -u "$1" -o pid,user,comm
+
