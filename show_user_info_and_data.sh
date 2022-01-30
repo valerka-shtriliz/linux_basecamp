bb61aa0583e0ceca4557649f18da4559387c16a8
vashvets <vashvets@0utlook.com>
2022-01-30 13:58:52 +0000

    modify script - added argument checking.

--- show_user_info_and_data.sh
+++ show_user_info_and_data.sh
@@ -3,6 +3,16 @@
 USERNAME=$1
 DIR_PATH=$2
 
+if [ $# -eq 0 ]; then
+echo "No arguments"
+exit
+fi
+
+if [ -z "$2" ]; then
+echo "No argument 2"
+exit
+fi
+
 case $1 in
 root)
 echo "Finding “root” user data is not allowed!"
@@ -10,8 +20,13 @@ exit 42 ;;
 *)
 echo "Директории пользователя" $1
 esac 
-PROC=$(find $2 -user $1)
-echo $PROC is found!
+DIR=$(find $2 -user $1)
+#echo $DIR is found!
+
+for DIR2 in $DIR 
+do 
+echo "$DIR2 is found!"
+done
 echo "Процессы пользователя"  $1
 
 ps -u "$1" -o pid,user,comm
